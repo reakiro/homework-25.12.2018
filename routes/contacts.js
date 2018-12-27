fc5b007e1b8db346d69693e5858d2a0b7f9e067e
reakiro <platypusdarina@gmail.com>
2018-12-27 15:14:46 +0200

    all working

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'platypusdarina@gmail.com',
+    pass: 'Dfyzcfafhjd2'
+  }
+});
 
 /* GET contacts page. */
 router.get('/', function(req, res, next) {
-  res.render('contacts', { title: 'contacts'});
+  res.render('contacts', { title: 'contacts' });
+});
+
+router.post('/', function(req, res) {
+	var name = req.body.name;
+	var number = req.body.number;
+	var email = req.body.email;
+
+	var transporter = nodemailer.createTransport({
+	  service: 'gmail',
+	  auth: {
+	    user: 'email@gmail.com',
+	    pass: 'pass'
+	  }
+	});
+
+	var mailOptions = {
+	  from: 'email@gmail.com',
+	  to: 'email@gmail.com',
+	  subject: 'data from superexpresswebsite',
+	  text: 'name: ' + name + '\nnumber: ' + number + '\nemail: ' + email
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+	  if (error) {
+	    console.log(error);
+	  } else {
+	    console.log('Email sent: ' + info.response);
+	  }
+	});
+	
+	res.redirect('/contacts');
 });
 
 module.exports = router;
